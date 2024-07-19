895a3753d0a3124c8962ca3316ee5710c2c73dc4
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2024-07-20 00:08:13 +0200

    Text container fix

--- lib/styles/formToggle.ts
+++ lib/styles/formToggle.ts
@@ -177,6 +177,7 @@ export const formToggle = css`
         display: flex;
         flex-direction: column;
         align-items: flex-start;
+        justify-content: center;
         gap: ${varSize("form-toggle-row-gap")};
         color: hsla(
             var(--form-field-label-color-h),
